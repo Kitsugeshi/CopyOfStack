f0cfe4c774813830906f46208dd76a8621bdcbb5
OkruzhkoVS <OkruzhkoVS@310D-7>
2023-09-08 16:03:25 +0700

    Добавьте файлы проекта.

---
+++ CopyOfStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CopyOfStack
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyStack<int> stack = new MyStack<int>(4);
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(3);
+            stack.Push(2);
+            //stack.Push(1);
+
+            var copy1 = new MyStack<int>(stack);
+            //copy1.Push(3);
+            Console.WriteLine(copy1.Peek());
+            copy1.Pop();
+            Console.WriteLine(copy1.Peek());
+            copy1.Pop();
+            Console.WriteLine(copy1.Peek());
+            copy1.Pop();
+            Console.WriteLine(copy1.Peek());
+        }
+    }
+}
---
+++ CopyOfStack/Stack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyOfStack
+{
+    class MyStack<T>
+    {
+        private List<T> items;
+        private int Size { get; set; }
+        public int Count { get { return items.Count; } }
+
+        public MyStack(int size) 
+        {
+            items = new List<T>(size);
+            Size = size;
+        }
+
+        public MyStack(MyStack<T> obj) //Конструктор копирования
+        {
+            this.items = obj.items;
+            obj.Size = Size;
+        }
+
+        public void Push(T item)
+        {
+            if (Count < Size)
+            {
+                items.Add(item);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Стек переполнен");
+            }
+        }
+
+        public T Pop()
+        {
+            if (Count > 0)
+            {
+                var item = items.LastOrDefault();
+                items.Remove(item);
+                return item;
+            }
+            else
+            {
+                throw new NullReferenceException("Стек пуст");
+            }
+        }
+
+        public T Peek()
+        {
+            if(Count > 0)
+            {
+                return items.LastOrDefault();
+            }
+            else 
+            { 
+                throw new NullReferenceException("Стек пуст");
+            }
+        }
+    }
+}
